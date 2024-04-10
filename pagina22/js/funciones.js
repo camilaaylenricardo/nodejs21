1be66e4751cb5dc84c1d65ee9f3d9edcfdfbb305
camilaaylenricardo <163005132+camilaaylenricardo@users.noreply.github.com>
2024-04-10 20:09:07 -0300

    Delete pagina22 directory

--- pagina22/js/funciones.js
+++
@@ -1,8 +0,0 @@
-function agregarPunto() {
-    let lista = document.getElementById("lista1")
-    let obj = lista.firstElementChild
-    while (obj != null) {
-        obj.innerText = obj.innerText + "."
-        obj = obj.nextElementSibling
-    }
-}
